db8117c38c30e325fcc98e4c3fe25d0823716890
kosenda <kkkk0630@ezweb.ne.jp>
2022-11-19 10:56:29 +0900

    apply kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,13 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.3.1'
-        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10'
-        classpath 'com.google.dagger:hilt-android-gradle-plugin:2.43.2'
-        classpath 'com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1'
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.create<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
