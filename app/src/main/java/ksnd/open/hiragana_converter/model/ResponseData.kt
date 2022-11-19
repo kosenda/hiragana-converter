888bc1a97384da0cf4fa49357e1bfe0b21bc8ad1
kosenda <kkkk0630@ezweb.ne.jp>
2022-11-19 09:57:12 +0900

    apply @Json

--- app/src/main/java/ksnd/open/hiragana_converter/model/ResponseData.kt
+++ app/src/main/java/ksnd/open/hiragana_converter/model/ResponseData.kt
@@ -1,16 +1,20 @@
 package ksnd.open.hiragana_converter.model
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = false)
 data class ResponseData(
 
     /** リクエストID */
-    val request_id: String,
+    @Json(name = "request_id")
+    val requestId: String,
 
     /** 出力タイプ */
-    val output_type: String,
+    @Json(name = "output_type")
+    val outputType: String,
 
     /** 変換後文字列 */
+    @Json(name = "converted")
     val converted: String
 )
