d899d0bcdd384d0e803c4125ef7a672f12d44ce4
kosenda <kkkk0630@ezweb.ne.jp>
2022-11-24 18:00:26 +0900

    change injecting datastore -> injecting repository in settingViewModel

--- app/src/main/java/ksnd/open/hiraganaconverter/model/repository/DataStoreRepository.kt
+++ app/src/main/java/ksnd/open/hiraganaconverter/model/repository/DataStoreRepository.kt
@@ -1,10 +1,13 @@
 package ksnd.open.hiraganaconverter.model.repository
 
 import kotlinx.coroutines.flow.Flow
+import ksnd.open.hiraganaconverter.view.CustomFont
 
 interface DataStoreRepository {
     fun selectedThemeNum(): Flow<Int>
     fun selectedCustomFont(): Flow<String>
+    fun updateThemeNum(newThemeNum: Int)
+    fun updateCustomFont(newCustomFont: CustomFont)
     fun lastConvertTime(): Flow<String>
     fun convertCount(): Flow<Int>
     fun updateLastConvertTime(lastConvertTime: String)
--- app/src/main/java/ksnd/open/hiraganaconverter/viewmodel/SettingsViewModelImpl.kt
+++ app/src/main/java/ksnd/open/hiraganaconverter/viewmodel/SettingsViewModelImpl.kt
@@ -1,86 +1,56 @@
 package ksnd.open.hiraganaconverter.viewmodel
 
-import android.util.Log
 import androidx.compose.runtime.mutableStateOf
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.emptyPreferences
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
-import ksnd.open.hiraganaconverter.model.PreferenceKeys
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import ksnd.open.hiraganaconverter.model.repository.DataStoreRepository
 import ksnd.open.hiraganaconverter.view.CustomFont
 import ksnd.open.hiraganaconverter.view.ThemeNum
-import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModelImpl @Inject constructor(
-    private val preferencesDataStore: DataStore<Preferences>
+    private val dataStoreRepository: DataStoreRepository
 ) : SettingsViewModel() {
-
-    private val tag = SettingsViewModelImpl::class.java.simpleName
 
     override val themeNum = mutableStateOf(ThemeNum.AUTO.num)
     override val customFont = mutableStateOf(CustomFont.DEFAULT.name)
 
+    private val customFontFlow: StateFlow<String> = dataStoreRepository
+        .selectedCustomFont()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Eagerly,
+            initialValue = CustomFont.DEFAULT.name
+        )
+
+    private val themeNumFlow: StateFlow<Int> = dataStoreRepository
+        .selectedThemeNum()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Eagerly,
+            initialValue = ThemeNum.AUTO.num
+        )
+
     override fun updateThemeNum(newThemeNum: Int) {
-        CoroutineScope(Dispatchers.IO).launch {
-            preferencesDataStore.edit { it[PreferenceKeys.THEME_NUM] = newThemeNum }
-            themeNum.value = newThemeNum
-        }
+        themeNum.value = newThemeNum
+        dataStoreRepository.updateThemeNum(newThemeNum)
     }
 
     override fun updateCustomFont(newCustomFont: CustomFont) {
-        CoroutineScope(Dispatchers.IO).launch {
-            preferencesDataStore.edit { it[PreferenceKeys.CUSTOM_FONT] = newCustomFont.name }
-            customFont.value = newCustomFont.name
-        }
+        customFont.value = newCustomFont.name
+        dataStoreRepository.updateCustomFont(newCustomFont)
     }
 
     override fun getThemeNum() {
-        CoroutineScope(Dispatchers.IO).launch {
-            val customFontFlow: Flow<Int> = preferencesDataStore.data
-                .catch { exception ->
-                    Log.e(tag, "preferencesDataStore $exception")
-                    if (exception is IOException) {
-                        emit(emptyPreferences())
-                    } else {
-                        ThemeNum.AUTO.num
-                    }
-                }
-                .map { preferences ->
-                    preferences[PreferenceKeys.THEME_NUM] ?: ThemeNum.AUTO.num
-                }
-            customFontFlow.collect {
-                themeNum.value = it
-            }
-        }
+        themeNum.value = themeNumFlow.value
     }
 
     override fun getCustomFont() {
-        CoroutineScope(Dispatchers.IO).launch {
-            val customFontFlow: Flow<String> = preferencesDataStore.data
-                .catch { exception ->
-                    Log.e(tag, "preferencesDataStore $exception")
-                    if (exception is IOException) {
-                        emit(emptyPreferences())
-                    } else {
-                        CustomFont.DEFAULT.name
-                    }
-                }
-                .map { preferences ->
-                    preferences[PreferenceKeys.CUSTOM_FONT] ?: CustomFont.DEFAULT.name
-                }
-            customFontFlow.collect {
-                customFont.value = it
-            }
-        }
+        customFont.value = customFontFlow.value
     }
 
     override fun isSelectedThemeNum(index: Int): Boolean {
