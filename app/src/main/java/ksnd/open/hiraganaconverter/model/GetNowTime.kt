07b9a88e9bf91a4df2c85149a67a5df817358a1b
ksnd <kkkk6311@yahoo.co.jp>
2022-12-18 22:26:36 +0900

    change param in getNowTime

--- app/src/main/java/ksnd/open/hiraganaconverter/model/GetNowTime.kt
+++ app/src/main/java/ksnd/open/hiraganaconverter/model/GetNowTime.kt
@@ -4,9 +4,14 @@ import android.text.format.DateFormat
 import java.util.Calendar
 import java.util.TimeZone
 
-fun getNowTime(timeZone: String, format: String = "yyyy/MM/dd HH:mm"): String {
+enum class TimeFormat(val text: String) {
+    YEAR_MONTH_DATE_HOUR_MINUTE(text = "yyyy/MM/dd HH:mm"),
+    YEAR_MONTH_DATE(text = "yyyy-MM-dd"),
+}
+
+fun getNowTime(timeZone: String, format: TimeFormat): String {
     return DateFormat.format(
-        format,
+        format.text,
         Calendar.getInstance(
             if ("default" == timeZone) {
                 TimeZone.getDefault()
