1f67923f6012d6a53d9cae3f4a9adcdac6698c65
ksnd <kkkk6311@yahoo.co.jp>
2023-02-25 00:46:41 +0900

    desing: change BottomCloseButton Button to OutlinedButton

--- app/src/main/java/ksnd/open/hiraganaconverter/view/parts/button/BottomCloseButton.kt
+++ app/src/main/java/ksnd/open/hiraganaconverter/view/parts/button/BottomCloseButton.kt
@@ -4,20 +4,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,17 +30,14 @@ fun BottomCloseButton(
     onClick: () -> Unit,
 ) {
     val buttonScaleState = rememberButtonScaleState()
-    Button(
+    OutlinedButton(
         modifier = modifier
             .fillMaxWidth(1f)
             .padding(horizontal = 16.dp)
             .padding(bottom = 16.dp)
             .height(56.dp)
-            .scale(scale = buttonScaleState.animationScale.value)
-            .shadow(elevation = 4.dp, shape = CircleShape),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = MaterialTheme.colorScheme.tertiaryContainer,
-        ),
+            .scale(scale = buttonScaleState.animationScale.value),
+        colors = ButtonDefaults.outlinedButtonColors(containerColor = Color.Transparent),
         shape = RoundedCornerShape(50),
         onClick = onClick,
         interactionSource = buttonScaleState.interactionSource,
