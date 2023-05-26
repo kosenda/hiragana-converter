d6c24b964b6d62418489d5a6f031bb6b04150cc6
kosenda <ksnd@KSND-MacBook-Pro.local>
2023-05-27 01:47:20 +0900

    feature: versionUp libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.1")
+        classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.46.1")
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
     }
