bf386888fd0035c2b98ab08e582e698fd19788a7
kosenda <ksnd@KSND-MacBook-Pro.local>
2023-07-16 16:24:07 +0900

    feature: delete buildScript in root-build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,17 +9,3 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.secrets) apply false
 }
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0-rc01")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.46.1")
-        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
-    }
-}
