2be8708c024b47b5e7f40086659901a55d1c8dee
kosenda <ksnd@KSND-MacBook-Pro.local>
2023-07-19 00:40:14 +0900

    fix: change debugimplement to implement for preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     // Compose
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
-    debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.ui.tooling.preview)
 
     // COIL
