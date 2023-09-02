788101c988591a425e8b05408e3fa85d89705724
kosenda <kkkk0630@ezweb.ne.jp>
2023-09-02 16:04:28 +0900

    feature: migrate to EdgeToEdge.enableEdgeToEdge

--- app/src/main/java/ksnd/hiraganaconverter/view/screen/ConverterScreen.kt
+++ app/src/main/java/ksnd/hiraganaconverter/view/screen/ConverterScreen.kt
@@ -35,7 +35,6 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -58,11 +57,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import ksnd.hiraganaconverter.R
 import ksnd.hiraganaconverter.model.ConvertErrorType
 import ksnd.hiraganaconverter.model.repository.LIMIT_CONVERT_COUNT
-import ksnd.hiraganaconverter.view.LocalIsDark
 import ksnd.hiraganaconverter.view.parts.TopBar
 import ksnd.hiraganaconverter.view.parts.button.ConvertButton
 import ksnd.hiraganaconverter.view.parts.button.CustomButtonWithBackground
@@ -82,13 +79,6 @@ fun ConverterScreen(
     snackbarHostState: SnackbarHostState,
     convertViewModel: ConvertViewModelImpl,
 ) {
-    val systemUiController = rememberSystemUiController()
-    val isDarkTheme = LocalIsDark.current
-
-    SideEffect {
-        systemUiController.setSystemBarsColor(color = Color.Transparent, darkIcons = isDarkTheme.not())
-    }
-
     ConverterScreenContent(
         modifier = modifier,
         viewModel = convertViewModel,
