73fd87ceb42010992247a89e425813e5dc9f10c2
kosenda <kkkk0630@ezweb.ne.jp>
2023-09-03 16:06:31 +0900

    feature: add haptics in button

--- app/src/main/java/ksnd/hiraganaconverter/view/extension/NoRippleClickable.kt
+++ app/src/main/java/ksnd/hiraganaconverter/view/extension/NoRippleClickable.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.semantics.Role
 
 inline fun Modifier.noRippleClickable(
