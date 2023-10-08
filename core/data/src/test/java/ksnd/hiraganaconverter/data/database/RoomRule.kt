4d917c259ddeaee455ec85a89b280720e3eb3d12
kosenda <kkkk0630@ezweb.ne.jp>
2023-10-08 09:37:27 +0900

    refacotr: use dao in ConvertHistoryRepositoryImplTest

---
+++ core/data/src/test/java/ksnd/hiraganaconverter/data/database/RoomRule.kt
@@ -0,0 +1,19 @@
+package ksnd.hiraganaconverter.data.database
+
+import android.content.Context
+import androidx.room.Room
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class RoomRule(context: Context) : TestWatcher() {
+    private var database: ConvertHistoryDatabase = Room
+        .databaseBuilder(context = context, ConvertHistoryDatabase::class.java, "test")
+        .allowMainThreadQueries()
+        .build()
+
+    val dao: ConvertHistoryDao = database.convertHistoryDao()
+
+    override fun finished(description: Description) {
+        database.close()
+    }
+}
