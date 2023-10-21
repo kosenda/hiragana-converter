f831958b2f97868236a939ef64c6a1068b18e86f
kosenda <kkkk0630@ezweb.ne.jp>
2023-10-21 14:32:13 +0900

    refactor: remove ktx from firebase

--- app/src/main/java/ksnd/hiraganaconverter/view/Application.kt
+++ app/src/main/java/ksnd/hiraganaconverter/view/Application.kt
@@ -2,8 +2,8 @@ package ksnd.hiraganaconverter.view
 
 import android.app.Application
 import android.util.Log
-import com.google.firebase.crashlytics.ktx.crashlytics
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.Firebase
+import com.google.firebase.crashlytics.crashlytics
 import dagger.hilt.android.HiltAndroidApp
 import ksnd.hiraganaconverter.BuildConfig
 import timber.log.Timber
