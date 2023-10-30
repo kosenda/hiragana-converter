a801b7010082b1b8265169448b2a450dc9a72374
kosenda <kkkk0630@ezweb.ne.jp>
2023-10-31 00:55:31 +0900

    feature: add ref

--- core/analytics/src/main/java/ksnd/hiraganaconverter/core/analytics/di/AnalyticModule.kt
+++ core/analytics/src/main/java/ksnd/hiraganaconverter/core/analytics/di/AnalyticModule.kt
@@ -12,6 +12,9 @@ import ksnd.hiraganaconverter.core.analytics.Analytics
 import ksnd.hiraganaconverter.core.analytics.AnalyticsImpl
 import javax.inject.Singleton
 
+/**
+ * ref: https://github.com/android/nowinandroid/blob/15f8861da15ff75876af0f1383d23417eb79d89e/core/analytics/src/prod/kotlin/com/google/samples/apps/nowinandroid/core/analytics/AnalyticsModule.kt
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class AnalyticModule {
