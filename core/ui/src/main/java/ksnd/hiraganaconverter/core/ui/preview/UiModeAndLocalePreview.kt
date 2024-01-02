177b77af5bf60e0bd6dbd9a0c9482b26480764ef
kosenda <kkkk0630@ezweb.ne.jp>
2024-01-02 15:30:58 +0900

    chore: rename preview group

--- core/ui/src/main/java/ksnd/hiraganaconverter/core/ui/preview/UiModeAndLocalePreview.kt
+++ core/ui/src/main/java/ksnd/hiraganaconverter/core/ui/preview/UiModeAndLocalePreview.kt
@@ -3,6 +3,6 @@ package ksnd.hiraganaconverter.core.ui.preview
 import android.content.res.Configuration
 import androidx.compose.ui.tooling.preview.Preview
 
-@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES, group = "UiModePreview")
-@Preview(name = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO, group = "UiModePreview")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES, group = "ui")
+@Preview(name = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO, group = "ui")
 annotation class UiModePreview
