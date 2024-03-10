c881011f92dc470d6052b015b68e78a21f17c2c1
kosenda <kkkk0630@ezweb.ne.jp>
2024-03-10 16:47:03 +0900

    test: change clip in ErrorCard

--- core/ui/src/main/java/ksnd/hiraganaconverter/core/ui/parts/card/ErrorCard.kt
+++ core/ui/src/main/java/ksnd/hiraganaconverter/core/ui/parts/card/ErrorCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -17,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
@@ -48,6 +50,7 @@ fun ErrorCard(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(all = 16.dp)
+                .clip(CircleShape)
                 .scale(scale = buttonScaleState.animationScale.value)
                 .noRippleClickable(
                     interactionSource = buttonScaleState.interactionSource,
