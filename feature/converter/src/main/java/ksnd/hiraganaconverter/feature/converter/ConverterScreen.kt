1c78415495f58a46975828bcbe07f2c0bb936e8b
kosenda <kkkk0630@ezweb.ne.jp>
2024-03-30 17:16:08 +0900

    test: remove spacer

--- feature/converter/src/main/java/ksnd/hiraganaconverter/feature/converter/ConverterScreen.kt
+++ feature/converter/src/main/java/ksnd/hiraganaconverter/feature/converter/ConverterScreen.kt
@@ -189,7 +189,6 @@ fun ConverterScreenContent(
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
                     ConversionTypeCard(onSelectedChange = changeHiraKanaType)
-                    Spacer(modifier = Modifier.weight(1f))
                     CustomButtonWithBackground(
                         id = R.drawable.ic_reset,
                         convertDescription = "reset",
