e4f1ad01b3d36be2063ec3fe285f3e22f24f1bb4
kosenda <kkkk0630@ezweb.ne.jp>
2024-05-25 19:16:51 +0900

    chore: delete unused import

--- feature/setting/src/main/java/ksnd/hiraganaconverter/feature/setting/section/SettingThemeSection.kt
+++ feature/setting/src/main/java/ksnd/hiraganaconverter/feature/setting/section/SettingThemeSection.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ksnd.hiraganaconverter.core.model.ui.FontType
 import ksnd.hiraganaconverter.core.model.ui.Theme
