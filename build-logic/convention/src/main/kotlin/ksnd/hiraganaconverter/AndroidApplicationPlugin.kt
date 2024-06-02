4fe57b874a53a93549cae4f6ab8f6b050581c03c
kosenda <kkkk0630@ezweb.ne.jp>
2024-06-02 23:39:58 +0900

    chore: restore comment option

--- build-logic/convention/src/main/kotlin/ksnd/hiraganaconverter/AndroidApplicationPlugin.kt
+++ build-logic/convention/src/main/kotlin/ksnd/hiraganaconverter/AndroidApplicationPlugin.kt
@@ -4,9 +4,7 @@ import com.android.build.api.dsl.ApplicationExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.getByType
 
 class AndroidApplicationPlugin : Plugin<Project> {
     override fun apply(target: Project) {
