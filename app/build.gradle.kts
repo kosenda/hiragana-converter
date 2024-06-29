af2fbb4077cd2d305514f2395c5dd691d0384aed
kosenda <kkkk0630@ezweb.ne.jp>
2024-06-29 14:47:36 +0900

    feature: versionUp v1.32(43)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "ksnd.hiraganaconverter"
         minSdk = 26
         targetSdk = 34
-        versionCode = 42
-        versionName = "1.31"
+        versionCode = 43
+        versionName = "1.32"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     compileOptions {
