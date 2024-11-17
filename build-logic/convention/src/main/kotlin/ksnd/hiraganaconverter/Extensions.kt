7772a5d89518653d71aa072b10e73e78417f3630
kosenda <kkkk0630@ezweb.ne.jp>
2024-11-17 10:46:57 +0900

    feature: change jvmTarget and strongSkipMode

--- build-logic/convention/src/main/kotlin/ksnd/hiraganaconverter/Extensions.kt
+++ build-logic/convention/src/main/kotlin/ksnd/hiraganaconverter/Extensions.kt
@@ -1,23 +1,13 @@
 package ksnd.hiraganaconverter
 
-import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.withType
 import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
 import org.gradle.testing.jacoco.plugins.JacocoTaskExtension
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
-
-/*
- * ref: https://github.com/android/nowinandroid/blob/main/build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/KotlinAndroid.kt
- */
-fun CommonExtension<*, *, *, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
-    (this as ExtensionAware).extensions.configure("kotlinOptions", block)
-}
 
 /*
  * ref: https://github.com/android/nowinandroid/blob/main/build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/Jacoco.kt
