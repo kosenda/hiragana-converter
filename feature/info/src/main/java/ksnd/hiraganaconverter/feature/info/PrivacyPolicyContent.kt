348edd46721f767c7372872c7aefa62393e2c63f
kosenda <kkkk0630@ezweb.ne.jp>
2024-12-22 16:15:34 +0900

    bug-fix: add statusBarsPadding in PrivacyPolicy

--- feature/info/src/main/java/ksnd/hiraganaconverter/feature/info/PrivacyPolicyContent.kt
+++ feature/info/src/main/java/ksnd/hiraganaconverter/feature/info/PrivacyPolicyContent.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.BottomSheetDefaults
@@ -83,6 +84,7 @@ fun PrivacyPolicyContent() {
             onDismissRequest = { isShowWebView = false },
             dragHandle = { DragHandle(navigator = navigator) },
             sheetState = sheetState,
+            modifier = Modifier.statusBarsPadding()
         ) {
             Box(
                 modifier = Modifier
