7dbcc26db1012a60659fc29f347661db17b158cb
kosenda <kkkk0630@ezweb.ne.jp>
2024-12-29 13:59:48 +0900

    feature: change to register

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
     alias(libs.plugins.aboutLibraries) apply false
 }
 
-tasks.create<JacocoReport>("jacocoTestReport") {
+tasks.register<JacocoReport>("jacocoTestReport") {
     reports {
         html.required.set(true)
         xml.required.set(true)
@@ -92,7 +92,7 @@ dependencies {
     }
 }
 
-tasks.create<JavaExec>("ktlintCheck") {
+tasks.register<JavaExec>("ktlintCheck") {
     description = "Check Kotlin code style."
     classpath = ktlint
     mainClass.set("com.pinterest.ktlint.Main")
@@ -103,7 +103,7 @@ tasks.create<JavaExec>("ktlintCheck") {
     isIgnoreExitValue = true
 }
 
-tasks.create<JavaExec>("ktlintFormatting") {
+tasks.register<JavaExec>("ktlintFormatting") {
     description = "Fix Kotlin code style deviations."
     classpath = ktlint
     mainClass.set("com.pinterest.ktlint.Main")
