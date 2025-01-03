af0cd390e52c43a86bce510d4e37aad834cb5d01
kosenda <kkkk0630@ezweb.ne.jp>
2025-01-03 11:17:05 +0900

    feature: change modifier in TopBar

--- core/ui/src/main/java/ksnd/hiraganaconverter/core/ui/parts/TopBar.kt
+++ core/ui/src/main/java/ksnd/hiraganaconverter/core/ui/parts/TopBar.kt
@@ -1,16 +1,12 @@
 package ksnd.hiraganaconverter.core.ui.parts
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.calculateEndPadding
-import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBar
@@ -18,12 +14,11 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.dropUnlessResumed
 import ksnd.hiraganaconverter.core.resource.R
@@ -40,58 +35,48 @@ fun TopBar(
     modifier: Modifier = Modifier,
     navigateScreen: (Nav) -> Unit,
 ) {
-    val layoutDirection = LocalLayoutDirection.current
-    val isShowTopBar by remember(scrollBehavior.state.collapsedFraction) {
-        derivedStateOf { scrollBehavior.state.collapsedFraction != 1.toFloat() }
+    // Allow content to be displayed at the statusBar when scrolling
+    val statusBarHeight = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
+    val variableTopPaddingHeight by remember(scrollBehavior.state.collapsedFraction) {
+        mutableStateOf(statusBarHeight * (1f - scrollBehavior.state.collapsedFraction))
     }
 
-    AnimatedVisibility(
-        visible = isShowTopBar,
-        modifier = modifier.padding(
-            start = WindowInsets.displayCutout
-                .asPaddingValues()
-                .calculateStartPadding(layoutDirection),
-            end = WindowInsets.displayCutout
-                .asPaddingValues()
-                .calculateEndPadding(layoutDirection),
+    TopAppBar(
+        title = {},
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.surface)
+            .padding(top = variableTopPaddingHeight)
+            .consumeWindowInsets(WindowInsets.statusBars),
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color.Transparent,
+            scrolledContainerColor = Color.Transparent,
+            titleContentColor = MaterialTheme.colorScheme.primary,
         ),
-        enter = fadeIn(),
-        exit = fadeOut(),
-    ) {
-        TopAppBar(
-            title = {},
-            modifier = Modifier.background(MaterialTheme.colorScheme.surface),
-            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                containerColor = Color.Transparent,
-                scrolledContainerColor = Color.Transparent,
-                titleContentColor = MaterialTheme.colorScheme.primary,
-            ),
-            actions = {
-                CustomIconButton(
-                    icon = R.drawable.ic_outline_info_24,
-                    contentDescription = "",
-                    modifier = Modifier.padding(horizontal = 8.dp),
-                    onClick = dropUnlessResumed { navigateScreen(Nav.Info) },
-                )
-                CustomIconButton(
-                    icon = R.drawable.ic_outline_settings_24,
-                    contentDescription = "",
-                    modifier = Modifier.padding(end = 8.dp),
-                    onClick = dropUnlessResumed { navigateScreen(Nav.Setting) },
-                )
-                CustomIconButton(
-                    icon = R.drawable.ic_baseline_history_24,
-                    contentDescription = "",
-                    onClick = dropUnlessResumed { navigateScreen(Nav.History) },
-                )
-                Spacer(modifier = Modifier.weight(1f))
-                if (isTest().not()) {
-                    GooCreditImage()
-                }
-            },
-            scrollBehavior = scrollBehavior,
-        )
-    }
+        actions = {
+            CustomIconButton(
+                icon = R.drawable.ic_outline_info_24,
+                contentDescription = "",
+                modifier = Modifier.padding(horizontal = 8.dp),
+                onClick = dropUnlessResumed { navigateScreen(Nav.Info) },
+            )
+            CustomIconButton(
+                icon = R.drawable.ic_outline_settings_24,
+                contentDescription = "",
+                modifier = Modifier.padding(end = 8.dp),
+                onClick = dropUnlessResumed { navigateScreen(Nav.Setting) },
+            )
+            CustomIconButton(
+                icon = R.drawable.ic_baseline_history_24,
+                contentDescription = "",
+                onClick = dropUnlessResumed { navigateScreen(Nav.History) },
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            if (isTest().not()) {
+                GooCreditImage()
+            }
+        },
+        scrollBehavior = scrollBehavior,
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
